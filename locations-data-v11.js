c75d1955764d9d19b88dd309c00dd3a928718f12
Alfonso-de-Celis <alfonso.decelis@aenus.es>
2026-09-16 16:04:59 +0200

    Add validated FROMAGO venue coordinates

---
+++ locations-data-v11.js
@@ -0,0 +1,35 @@
+(() => {
+'use strict';
+if(window.FROMAGO_GEO_DATA)return;
+window.FROMAGO_GEO_DATA={
+ points:{
+  L01:{name:'Mirador Claudio Moyano',lat:41.5018611,lng:-5.7480278},
+  L02:{name:'Trascastillo · Bestiarium',lat:41.4987500,lng:-5.7550278},
+  L03:{name:'Carpa Sabor a Málaga',lat:41.5072778,lng:-5.7403333},
+  L04:{name:'Palacio de la Encarnación',lat:41.5028889,lng:-5.7486389},
+  L05:{name:'Plaza de la Marina',lat:41.5072778,lng:-5.7403333},
+  L06:{name:'Jardines del Castillo',lat:41.4992778,lng:-5.7554167},
+  L07:{name:'Plaza Mayor',lat:41.5032500,lng:-5.7468611},
+  L08:{name:'Escenario Troncoso Privée',lat:41.4991389,lng:-5.7520000},
+  L09:{name:'Espacio Zamora es Calidad · Diputación',lat:41.5023056,lng:-5.7483333},
+  L10:{name:'Vaca Vallelongo',lat:41.5018611,lng:-5.7480278},
+  L11:{name:'Plaza de los Animales',lat:41.5015556,lng:-5.7528611},
+  L12:{name:'Stand 141-142 Diputación de Zamora',lat:41.5023056,lng:-5.7483333},
+  L13:{name:'Atrio de la Catedral',lat:41.4992778,lng:-5.7542778},
+  L14:{name:'Stand DOP Queso Zamorano · Rúa de los Francos',lat:41.5012500,lng:-5.7501389},
+  L15:{name:'Iglesia de San Cipriano',lat:41.5014167,lng:-5.7482778},
+  L16:{name:'Iglesia de San Juan de Puerta Nueva',lat:41.5032500,lng:-5.7468611},
+  L17:{name:'Plaza de la Lana',lat:41.5000556,lng:-5.7513889},
+  L18:{name:'Zona PanFest · Calle Viriato',lat:41.5037222,lng:-5.7456944},
+  L19:{name:'Teatro Ramos Carrión',lat:41.5025833,lng:-5.7475278},
+  L20:{name:'Plaza de la Constitución',lat:41.5048056,lng:-5.7441667},
+  L21:{name:'Plaza de los Momos',lat:41.5044722,lng:-5.7453333}
+ },
+ galleries:[
+  {name:'Nacional · Plaza de Viriato',lat:41.5023056,lng:-5.7483333},
+  {name:'Internacional · Catedral',lat:41.4992778,lng:-5.7542778},
+  {name:'Europea DOP-IGP · Arias Gonzalo',lat:41.4996111,lng:-5.7523056},
+  {name:'Premios Cincho · Plaza de la Marina',lat:41.5072778,lng:-5.7403333}
+ ]
+};
+})();
